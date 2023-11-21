5b16cc455687cbf9b97f1d24c49b84d2fdb785a4
Michelle Primiani <primianimichelle@gmail.com>
2023-11-21 10:07:53 -0600

    Created PostList on homepage and post content cards

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react"
-
+import "./PostList.css"
 
 export const PostList = () => {
     const [allPosts, setAllPosts] = useState([])
@@ -19,9 +19,29 @@ export const PostList = () => {
         fetchAllPosts()
     }, [])
 
-
-
     return (
-        <div>Hi!</div>
-    )
-}
+        <div className="background-wrapper">
+            <div className="container">
+                <h1 className="post-header">Posts</h1>
+                {allPosts.map((post) => (
+                    <div className="post-card" key={post.id}>
+                        <div className="card-content">
+                            <h2 className="post-title">{post.title}</h2>
+                            <div className="author-date">
+                                <span className="author">Author: {post.user.user.first_name} {post.user.user.last_name}</span>
+                                <span className="date">{post.pub_date}</span>
+                            </div>
+                            <img className="post-image" src={post.image_url} alt="Post" />
+                        </div>
+                        <div className="card-footer">
+                            <span className="reactions">{post.reaction_count} reaction count</span>
+                            <div className="card-actions">
+
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+                }    
