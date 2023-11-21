af29b295bd5895726cad6af31f1899e241951390
Luke Hardy <luke.taylor.hardy@gmail.com>
2023-11-21 12:53:25 -0500

    completed list view and create form for categories

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./CategoryList.css";
 
 export const CategoryList = () => {
+  const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
+  const initialCatState = {
+    label: "",
+  };
+  const [category, updateCategory] = useState(initialCatState);
 
-  const fetchCategories = async () => {
+  useEffect(() => {
+    fetchAndSetCategories();
+  }, []);
+
+  const fetchAndSetCategories = async () => {
     let url = "http://localhost:8000/categories";
 
     const response = await fetch(url, {
@@ -15,15 +26,24 @@ export const CategoryList = () => {
 
     setCategories(catArray);
   };
-  useEffect(() => {
-    fetchCategories();
-  }, []);
+
+  const createCategory = async (evt) => {
+    await fetch("http://localhost:8000/categories", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(category),
+    });
+    fetchAndSetCategories();
+  };
 
   const displayCategories = () => {
     if (categories && categories.length) {
       return categories.map((cat) => (
         <div key={cat.id}>
-          <div className="underline">{cat.label}</div>
+          <div className="cat-item">{cat.label}</div>
         </div>
       ));
     }
@@ -33,8 +53,32 @@ export const CategoryList = () => {
 
   return (
     <>
-      <h1 className="text-3xl">Category List</h1>
-      {displayCategories()}
+      <div className="comp-container">
+        <div className="categories">
+          <span className="header">Categories</span>
+          <div className="cat-list">{displayCategories()}</div>
+        </div>
+        <div className="cat-form">
+          <h1 className="header">Create a new Category</h1>
+          <fieldset className="mt-4">
+            <input
+              placeholder="Category Name"
+              id="label"
+              type="text"
+              onChange={(e) => {
+                const copy = { ...category };
+                copy.label = e.target.value;
+                updateCategory(copy);
+              }}
+              value={category.label}
+              className="cat-input"
+            />
+          </fieldset>
+          <button onClick={createCategory} className="button">
+            Create
+          </button>
+        </div>
+      </div>
     </>
   );
 };
