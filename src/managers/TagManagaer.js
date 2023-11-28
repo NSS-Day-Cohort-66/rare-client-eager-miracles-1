0cc6eb842c9d95559e2b24c0432aef7caede3987
Spencer Norris <Spencer.serv@gmail.com>
2023-11-28 15:21:07 -0600

    Cleaned up Tag module

--- src/managers/TagManagaer.js
+++ src/managers/TagManagaer.js
@@ -8,3 +8,35 @@ export const deleteTag = (tag) => {
     });
   };
 
+export const editTag = (tag) => {
+  return fetch(`http://localhost:8000/tags/${tag.id}`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(tag)
+  }).then((res) => res.json());
+};
+
+export const fetchAndSetTags = () => {
+
+  return fetch("http://localhost:8000/tags", {
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+      "Content-Type": "application/json",
+    },
+  }).then(res => res.json());
+};
+
+export const postNewTag = async (newTag) => {
+  await fetch("http://localhost:8000/tags", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("auth_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTag),
+    });
+
+}
