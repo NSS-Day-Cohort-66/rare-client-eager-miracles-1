d2f77888b097bd782ea6951de1daf123a877b398
Michelle Primiani <primianimichelle@gmail.com>
2023-11-28 13:54:41 -0600

    Cleaned up PostList and moved fetch calls to manager

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,48 +1,30 @@
 import { useState, useEffect} from "react"
 import "./PostList.css"
 import { useNavigate } from "react-router-dom"
+import { deletePost, fetchAllPostsAndFilterByDate } from "../../managers/PostManager"
 
 export const PostList = () => {
     const [allPosts, setAllPosts] = useState([])
     const navigate = useNavigate()
 
-    const fetchAllPosts = async () => {
-        const response = await fetch('http://localhost:8000/posts', {
-            headers: {
-                "Authorization": `Token ${localStorage.getItem("auth_token")}`,
-},
-        })
-        const posts = await response.json()
-        const approvedPosts = posts
-        .filter((post) => post.approved === true)
-        .sort((a, b) => new Date(b.pub_date) - new Date(a.pub_date));
-      setAllPosts(approvedPosts);
-    }
-
     useEffect(() => {
-        fetchAllPosts()
-    }, [])
-
-
-    const btnToCreate = () => {
+        const fetchData = async () => {
+          const approvedPosts = await fetchAllPostsAndFilterByDate();
+          setAllPosts(approvedPosts);
+        };
+        
+        fetchData();
+    
+        const handleDeletePost = (postId) => {
+          deletePost(postId).then(() => {
+            fetchData(); 
+          });
+        };
+      }, []); 
+    
+      const btnToCreate = () => {
         navigate('/create');
-    };
-
-    const deletePost = (post) => {
-        return fetch(`http://localhost:8000/posts/${post.id}`, {
-          method: "DELETE",
-          headers: {
-            Authorization: `Token ${localStorage.getItem("auth_token")}`,
-            "Content-Type": "application/json",
-          },
-        });
       };
-
-      const handleDeletePost = (post) => {
-        deletePost(post).then(() => {
-          fetchAllPosts()
-        })
-      }
     
       
     return (
--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -1,4 +1,4 @@
-export const getPosts = () => {
+export const getAllPosts = () => {
     return fetch("http://localhost:8000/posts", {
       headers: {
         Authorization: `Token ${
@@ -7,3 +7,28 @@ export const getPosts = () => {
       },
     }).then((res) => res.json());
   };
+
+
+export const fetchAllPostsAndFilterByDate = async () => {
+  const response = await fetch(`http://localhost:8000/posts`, {
+    headers: {
+      "Authorization": `Token ${localStorage.getItem("auth_token")}`,
+    },
+  });
+  const posts = await response.json();
+  const approvedPosts = posts
+    .filter((post) => post.approved === true)
+    .sort((a, b) => new Date(b.pub_date) - new Date(a.pub_date));
+  return approvedPosts;
+};
+
+export const deletePost = async (postId) => {
+  const response = await fetch(`http://localhost:8000/${postId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("auth_token")}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return response;
+};
