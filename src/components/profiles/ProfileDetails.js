4c92baeef1d8820166412eece5215ff3c0554579
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-12-01 12:01:25 -0600

    styling and data implemented for data in ProfileDetails.js

--- src/components/profiles/ProfileDetails.js
+++ src/components/profiles/ProfileDetails.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getUserDetailsByUserId } from "../../managers/ProfileManager";
+import "./ProfileDetails.css";
 
 export const ProfileDetails = () => {
   const [profileDetails, setProfileDetails] = useState({});
@@ -17,22 +18,27 @@ export const ProfileDetails = () => {
   }, []);
 
   return (
-    <div className="background-wrapper">
-      <div className="container">
-        <div className="user-card" key={profileDetails.id}>
-          <div className="card-header">
-            <div className="user-title-section">
-              <h2 className="user-title">{profileDetails.user?.full_name}</h2>
-            </div>
-            <img
-              className="user-image"
-              src={profileDetails.image_avatar}
-              alt="Post"
-            />
+    <div className="user-detail-wrapper">
+      <div className="user-card" key={profileDetails.id}>
+        <div className="user-header">
+          <div className="user-title-section">
+            <h2 className="user-title">{profileDetails.user?.full_name}</h2>
           </div>
-
+          <img
+            className="user-image"
+            src={profileDetails.image_avatar}
+            alt="user"
+          />
+        </div>
+        <div className="user-details-wrapper">
+          {" "}
           <div className="user-email">{profileDetails.user?.email}</div>
-          <div className="user-date-joined">{profileDetails.created_on}</div>
+          <div className="user-user-type">
+            {profileDetails.user?.user_profile_type.toUpperCase()}
+          </div>
+          <div className="user-date-joined">
+            Date Joined: {profileDetails.created_on}
+          </div>
         </div>
       </div>
     </div>
