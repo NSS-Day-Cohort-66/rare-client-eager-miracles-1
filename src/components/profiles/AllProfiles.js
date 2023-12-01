3f0e1aeeffb59df54a7d1908df27df4929610aee
Michelle Primiani <primianimichelle@gmail.com>
2023-12-01 11:31:14 -0600

    Created subscription manager and subscribe button

--- src/components/profiles/AllProfiles.js
+++ src/components/profiles/AllProfiles.js
@@ -2,17 +2,39 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AllProfiles.css";
 import { getAllUsers } from "../../managers/ProfileManager";
+import { createSubscription } from "../../managers/SubscriptionManager";
 
-export const ProfileList = () => {
+export const ProfileList = ( {userId}) => {
   const [allUsers, setAllUsers] = useState([]);
   const navigate = useNavigate();
 
   const fetchAndSetUsers = () => {
     getAllUsers().then((usersArray) => setAllUsers(usersArray));
   };
+
   useEffect(() => {
     fetchAndSetUsers();
   }, []);
+
+  const handleSubscribeClick = (authorId) => {
+    const followerId = parseInt(userId)
+    const newSub = {
+      author: authorId,
+      follower: followerId
+    };
+    createSubscription(newSub)
+      .then(() => {
+
+        alert('Subscription successful!');
+        fetchAndSetUsers();
+        alert('You are now subscribed - I will add a color once I figure out unsubscribe. Go to postman and make sure this works :)')
+      })
+      .catch((error) => {
+        console.error("Error subscribing to user:", error);
+        alert('Failed to subscribe.');
+      });
+  };
+
 
   return (
     <div className="all-profiles-container">
@@ -39,6 +61,7 @@ export const ProfileList = () => {
               src={user.image_avatar}
               alt="user avatar"
             />
+            <button onClick={(e) => { e.stopPropagation();handleSubscribeClick(user.id)}}>Subscribe</button>
           </div>
         </section>
       ))}
